711846504639de0b0c2ed0004966093c8bfc89de
Andrew Perez <aperez@E7450>
2015-11-19 10:47:18 -0700

    Added ability to see list of articles

--- spec/features/articles_list_spec.rb
+++ spec/features/articles_list_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe "Listing articles" do
   scenario "someone views the articles index" do
-    visit path_articles
+    visit articles_path
+
     expect(page).to have_content "Articles List"
   end
 end
